7157b8df88e2821c057320af2a7c886dc3395290
vitormarquesr <96251229+vitormarquesr@users.noreply.github.com>
2023-08-27 22:38:15 -0300

    Add tests for mixing_matrix

--- tests/testthat/test-time_evolution.R
+++ tests/testthat/test-time_evolution.R
@@ -12,3 +12,14 @@ test_that("unitary evolution works", {
   expect_equal(U_mix, (1/sqrt(2))*rbind(c(1, 1i), c(1i, 1)))
 
 })
+
+test_that("mixing matrix works", {
+  walk_K3 <- ctqwalk(matrix(c(0,1,1,1,0,1,1,1,0), nrow=3))
+
+  expect_equal(mixing_matrix(walk_K3, 2*pi/9), matrix(1/3, nrow=3, ncol=3))
+
+
+  expect_equal(colSums(mixing_matrix(walk_K3, 10)), c(1, 1, 1))
+  expect_equal(rowSums(mixing_matrix(walk_K3, 31)), c(1, 1, 1))
+
+})
