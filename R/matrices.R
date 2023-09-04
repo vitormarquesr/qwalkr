1db2fa5dc00f6652383e79294a0c825607294d3f
vitormarquesr <96251229+vitormarquesr@users.noreply.github.com>
2023-09-04 14:30:03 -0300

    Add cartesian

--- R/matrices.R
+++ R/matrices.R
@@ -1,5 +1,8 @@
 #' The All-Ones Matrix
 #'
+#' @description
+#' Returns the all-ones matrix of order `n`.
+#'
 #' @param n the order of the matrix.
 #'
 #' @returns A square matrix of order \eqn{n} in which every entry is
@@ -16,6 +19,9 @@ J <- function(n){
 }
 
 #' The Trace of a Matrix
+#'
+#' @description
+#' Computes the trace of a matrix `A`.
 #'
 #' @param A a square matrix.
 #'
@@ -35,6 +41,9 @@ tr <- function(A){
 
 #' The Trace Inner Product of Matrices
 #'
+#' @description
+#' Computes the trace inner product of two matrices `A` and `B`.
+#'
 #' @param A,B square matrices.
 #'
 #' @returns The trace inner product on \eqn{Mat_{n\ x\ n}(\mathbb{C})} is
@@ -51,6 +60,46 @@ tr <- function(A){
 #' trdot(A, B)
 trdot <- function(A, B){
   return (tr(Conj(t(A)) %*% B))
+}
+
+#' Adjacency Matrix of the Cartesian Product
+#'
+#' @description
+#' Returns the adjacency matrix of the cartesian product of two graphs
+#' given the adjacency matrix of each one, \eqn{G} and \eqn{H}.
+#'
+#' @param G adjacency matrix of the first graph.
+#' @param H adjacency matrix of the second graph. If not provided,
+#'   it takes the same value as `G`.
+#'
+#' @returns Let \eqn{A(G),\ A(H)} be the adjacency matrices of
+#'   the graphs \eqn{G,\ H} such that \eqn{|V(G)| = n} and \eqn{|V(H)| = m},
+#'   then the adjacency matrix of the cartesian product \eqn{G \times H} is
+#'   given by
+#'
+#'   \deqn{A(G \times H) = A(G) \otimes I_{m\ x\ m} + I_{n\ x\ n} \otimes A(H)}
+#'
+#' @export
+#'
+#' @examples
+#' P3 <- matrix(c(0,1,0,1,0,1,0,1,0), nrow=3)
+#' K3 <- matrix(c(0,1,1,1,0,1,1,1,0), nrow=3)
+#'
+#' # Return the adjacency matrix of P3 X K3
+#' cartesian(P3, K3)
+#'
+#' # Return the adjacency matrix of P3 X P3
+#' cartesian(P3)
+cartesian <- function(G, H=NULL){
+  n1 <- nrow(G)
+  if (is.null(H)){
+    H <- G
+  }
+  n2 <- nrow(H)
+
+  W <- kronecker(G, diag(n2)) + kronecker(diag(n1), H)
+
+  return (W)
 }
 
 
